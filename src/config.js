c61b8cafc43adcee46bd6f83d61c8cb6e66c45b8
Daniel Laufenberg <danlanfen26@gmail.com>
2020-11-06 03:08:01 -0800

    testing file reconfigured

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-    //API_ENDPOINT: 'https://last-minute-eats-server.herokuapp.com/api',
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://last-minute-eats-server.herokuapp.com/api',
+    //API_ENDPOINT: 'http://localhost:8000/api',
     EMAIL_SUBMITTED: 'email_submitted'
 }
   
